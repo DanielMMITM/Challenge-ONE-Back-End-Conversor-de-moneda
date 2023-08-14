7d0034af583447a46f5b1b246a4d9fc809def6c1
Edgar Martínez <edgar.danmm@gmail.com>
2023-08-14 16:10:05 -0600

    Se controlo una excepcion que finalizaba el programa al momento de presionar otro boton en la parte de indicar un valor o cantidad,

--- src/main/java/org/converter/Main.java
+++ src/main/java/org/converter/Main.java
@@ -23,117 +23,130 @@ public class Main {
                 String currencySelected = (String) JOptionPane.showInputDialog(null, "Selecciona la divisa a convertir",
                         "Divisas", JOptionPane.QUESTION_MESSAGE, null, currencyOptions, null);
                 String cantidad = "0";
-                do {
-                    cantidad = (String) JOptionPane.showInputDialog(null, "Ingresa la cantidad que deseas convertir",
-                            "Cantidad", JOptionPane.QUESTION_MESSAGE);
+                try{
+                    do {
+                        cantidad = (String) JOptionPane.showInputDialog(null, "Ingresa la cantidad que deseas convertir",
+                                "Cantidad", JOptionPane.QUESTION_MESSAGE);
+                    }
+                    while (!cantidad.matches("^[0-9]*.[0-9]*$"));
+                    switch (currencySelected) {
+                        case "Pesos MXN a Dólar":
+                            Moneda Dolar = new Moneda("Dolar", 0.059);
+                            double dolares = Dolar.doConversion(Double.parseDouble(cantidad));
+                            JOptionPane.showMessageDialog(null, Double.parseDouble(cantidad) +
+                                    " pesos equivale a " + dolares + " dolares");
+                            break;
+                        case "Pesos MXN a Euros":
+                            Moneda Euro = new Moneda("Euro", 0.054);
+                            double euros = Euro.doConversion(Double.parseDouble(cantidad));
+                            JOptionPane.showMessageDialog(null, Double.parseDouble(cantidad) +
+                                    " pesos equivale a " + euros + " euros");
+                            break;
+                        case "Pesos MXN a Libras Esterlinas":
+                            Moneda libraEsterlina = new Moneda("Libra Esterlina", 0.046);
+                            double librasEsterlinas = libraEsterlina.doConversion(Double.parseDouble(cantidad));
+                            JOptionPane.showMessageDialog(null, Double.parseDouble(cantidad) +
+                                    " pesos equivale a " + librasEsterlinas + " libras esterlinas");
+                            break;
+                        case "Pesos MXN a Yen Japonés":
+                            Moneda yenJapones = new Moneda("Yen Japonés", 8.51);
+                            double yensJaponeses = yenJapones.doConversion(Double.parseDouble(cantidad));
+                            JOptionPane.showMessageDialog(null, Double.parseDouble(cantidad) +
+                                    " pesos equivale a " + yensJaponeses + " yens japoneses");
+                            break;
+                        case "Pesos MXN a Won Surcoreano":
+                            Moneda wonSurcoreano = new Moneda("Won Surcoreano", 78.28);
+                            double wonsSurcoreanos = wonSurcoreano.doConversion(Double.parseDouble(cantidad));
+                            JOptionPane.showMessageDialog(null, Double.parseDouble(cantidad) +
+                                    " pesos equivale a " + wonsSurcoreanos + " wons surcoreanos");
+                            break;
+                        case "Dólar a Pesos MXN":
+                            pesoMXN.setValor(17.04);
+                            pesosMXN = pesoMXN.doConversion(Double.parseDouble(cantidad));
+                            JOptionPane.showMessageDialog(null, Double.parseDouble(cantidad) +
+                                    " dolares equivale a " + pesosMXN + " pesos mexicanos");
+                            break;
+                        case "Euros a Pesos MXN":
+                            pesoMXN.setValor(18.63);
+                            pesosMXN = pesoMXN.doConversion(Double.parseDouble(cantidad));
+                            JOptionPane.showMessageDialog(null, Double.parseDouble(cantidad) +
+                                    " euros equivale a " + pesosMXN + " pesos mexicanos");
+                            break;
+                        case "Libras Esterlinas a Pesos MXN":
+                            pesoMXN.setValor(21.58);
+                            pesosMXN = pesoMXN.doConversion(Double.parseDouble(cantidad));
+                            JOptionPane.showMessageDialog(null, Double.parseDouble(cantidad) +
+                                    " libras esterlinas equivale a " + pesosMXN + " pesos mexicanos");
+                            break;
+                        case "Yen Japonés a Pesos MXN":
+                            pesoMXN.setValor(0.12);
+                            pesosMXN = pesoMXN.doConversion(Double.parseDouble(cantidad));
+                            JOptionPane.showMessageDialog(null, Double.parseDouble(cantidad) +
+                                            " yens japoneses equivale a " + pesosMXN + " pesos mexicanos", "Yen Japonés a Pesos Mexicanos",
+                                    JOptionPane.YES_NO_CANCEL_OPTION);
+                            break;
+                        case "Won Surcoreano a Pesos MXN":
+                            pesoMXN.setValor(0.013);
+                            pesosMXN = pesoMXN.doConversion(Double.parseDouble(cantidad));
+                            JOptionPane.showMessageDialog(null, Double.parseDouble(cantidad) +
+                                    " wons surcoreanos   equivale a " + pesosMXN + " pesos mexicanos");
+                            break;
+                        default:
+                            JOptionPane.showMessageDialog(null, "No existe esa opcion");
+                    }
                 }
-                while (!cantidad.matches("^[0-9]*.[0-9]*$"));
-                switch (currencySelected) {
-                    case "Pesos MXN a Dólar":
-                        Moneda Dolar = new Moneda("Dolar", 0.059);
-                        double dolares = Dolar.doConversion(Double.parseDouble(cantidad));
-                        JOptionPane.showMessageDialog(null, Double.parseDouble(cantidad) +
-                                " pesos equivale a " + dolares + " dolares");
-                        break;
-                    case "Pesos MXN a Euros":
-                        Moneda Euro = new Moneda("Euro", 0.054);
-                        double euros = Euro.doConversion(Double.parseDouble(cantidad));
-                        JOptionPane.showMessageDialog(null, Double.parseDouble(cantidad) +
-                                " pesos equivale a " + euros + " euros");
-                        break;
-                    case "Pesos MXN a Libras Esterlinas":
-                        Moneda libraEsterlina = new Moneda("Libra Esterlina", 0.046);
-                        double librasEsterlinas = libraEsterlina.doConversion(Double.parseDouble(cantidad));
-                        JOptionPane.showMessageDialog(null, Double.parseDouble(cantidad) +
-                                " pesos equivale a " + librasEsterlinas + " libras esterlinas");
-                        break;
-                    case "Pesos MXN a Yen Japonés":
-                        Moneda yenJapones = new Moneda("Yen Japonés", 8.51);
-                        double yensJaponeses = yenJapones.doConversion(Double.parseDouble(cantidad));
-                        JOptionPane.showMessageDialog(null, Double.parseDouble(cantidad) +
-                                " pesos equivale a " + yensJaponeses + " yens japoneses");
-                        break;
-                    case "Pesos MXN a Won Surcoreano":
-                        Moneda wonSurcoreano = new Moneda("Won Surcoreano", 78.28);
-                        double wonsSurcoreanos = wonSurcoreano.doConversion(Double.parseDouble(cantidad));
-                        JOptionPane.showMessageDialog(null, Double.parseDouble(cantidad) +
-                                " pesos equivale a " + wonsSurcoreanos + " wons surcoreanos");
-                        break;
-                    case "Dólar a Pesos MXN":
-                        pesoMXN.setValor(17.04);
-                        pesosMXN = pesoMXN.doConversion(Double.parseDouble(cantidad));
-                        JOptionPane.showMessageDialog(null, Double.parseDouble(cantidad) +
-                                " dolares equivale a " + pesosMXN + " pesos mexicanos");
-                        break;
-                    case "Euros a Pesos MXN":
-                        pesoMXN.setValor(18.63);
-                        pesosMXN = pesoMXN.doConversion(Double.parseDouble(cantidad));
-                        JOptionPane.showMessageDialog(null, Double.parseDouble(cantidad) +
-                                " euros equivale a " + pesosMXN + " pesos mexicanos");
-                        break;
-                    case "Libras Esterlinas a Pesos MXN":
-                        pesoMXN.setValor(21.58);
-                        pesosMXN = pesoMXN.doConversion(Double.parseDouble(cantidad));
-                        JOptionPane.showMessageDialog(null, Double.parseDouble(cantidad) +
-                                " libras esterlinas equivale a " + pesosMXN + " pesos mexicanos");
-                        break;
-                    case "Yen Japonés a Pesos MXN":
-                        pesoMXN.setValor(0.12);
-                        pesosMXN = pesoMXN.doConversion(Double.parseDouble(cantidad));
-                        JOptionPane.showMessageDialog(null, Double.parseDouble(cantidad) +
-                                " yens japoneses equivale a " + pesosMXN + " pesos mexicanos", "Yen Japonés a Pesos Mexicanos",
-                                JOptionPane.YES_NO_CANCEL_OPTION);
-                        break;
-                    case "Won Surcoreano a Pesos MXN":
-                        pesoMXN.setValor(0.013);
-                        pesosMXN = pesoMXN.doConversion(Double.parseDouble(cantidad));
-                        JOptionPane.showMessageDialog(null, Double.parseDouble(cantidad) +
-                                " wons surcoreanos   equivale a " + pesosMXN + " pesos mexicanos");
-                        break;
+                catch (Exception ex){
+                    JOptionPane.showMessageDialog(null, "No puedes dejar el campo vacío");
                 }
             }
-            else {
+            else if(topicSelected == topicOptions[1]) {
                 double grados;
                 String temperatureSelected = (String) JOptionPane.showInputDialog(null,
                         "Selecciona la escala a convertir", "Temperaturas", JOptionPane.QUESTION_MESSAGE,
                         null, temperatureOptions, null);
                 String temperatura = "0";
-                do {
-                    temperatura = (String) JOptionPane.showInputDialog(null, "Ingresa la temperatura que deseas convertir",
-                            "Grados", JOptionPane.QUESTION_MESSAGE);
+                try {
+                    do {
+                        temperatura = (String) JOptionPane.showInputDialog(null, "Ingresa la temperatura que deseas convertir",
+                                "Grados", JOptionPane.QUESTION_MESSAGE);
+                    }
+                    while (!temperatura.matches("^-?[0-9]*.[0-9]*$"));
+                    switch (temperatureSelected){
+                        case "Grados Celsius a grados Farenheit":
+                            Temperatura Farenheit = new Temperatura("Farenheit", Double.parseDouble(temperatura));
+                            grados = Farenheit.doCelsiusToFarenheitConversion();
+                            JOptionPane.showMessageDialog(null, Double.parseDouble(temperatura) +
+                                    " grados Celsius equivalen a " + grados + " grados Farenheit");
+                            break;
+                        case "Grados Celsius a grados Kelvin":
+                            Temperatura Kelvin = new Temperatura("Kelvin", Double.parseDouble(temperatura));
+                            grados = Kelvin.doCelsiusToKelvinConversion();
+                            JOptionPane.showMessageDialog(null, Double.parseDouble(temperatura) +
+                                    " grados Celsius equivalen a " + grados + " grados Kelvin");
+                            break;
+                        case "Grados Farenheit a grados Celsius":
+                            Temperatura Celsius = new Temperatura("Celsius", Double.parseDouble(temperatura));
+                            grados = Celsius.doFarenheitToCelsiusConversion();
+                            JOptionPane.showMessageDialog(null, Double.parseDouble(temperatura) +
+                                    " grados Farenheit equivalen a " + grados + " grados Celsius");
+                            break;
+                        case "Grados Kelvin a grados Celsius":
+                            Temperatura Celsius2 = new Temperatura("Celsius", Double.parseDouble(temperatura));
+                            grados = Celsius2.doKelvinToCelsiusConversion();
+                            JOptionPane.showMessageDialog(null, Double.parseDouble(temperatura) +
+                                    " grados Kerlvin equivalen a " + grados + " grados Celsius");
+                            break;
+                        default:
+                            JOptionPane.showMessageDialog(null, "No existe esa opcion");
+                    }
                 }
-                while (!temperatura.matches("^-?[0-9]*.[0-9]*$"));
-                switch (temperatureSelected){
-                    case "Grados Celsius a grados Farenheit":
-                        Temperatura Farenheit = new Temperatura("Farenheit", Double.parseDouble(temperatura));
-                        grados = Farenheit.doCelsiusToFarenheitConversion();
-                        JOptionPane.showMessageDialog(null, Double.parseDouble(temperatura) +
-                                " grados Celsius equivalen a " + grados + " grados Farenheit");
-                        break;
-                    case "Grados Celsius a grados Kelvin":
-                        Temperatura Kelvin = new Temperatura("Kelvin", Double.parseDouble(temperatura));
-                        grados = Kelvin.doCelsiusToKelvinConversion();
-                        JOptionPane.showMessageDialog(null, Double.parseDouble(temperatura) +
-                                " grados Celsius equivalen a " + grados + " grados Kelvin");
-                        break;
-                    case "Grados Farenheit a grados Celsius":
-                        Temperatura Celsius = new Temperatura("Celsius", Double.parseDouble(temperatura));
-                        grados = Celsius.doFarenheitToCelsiusConversion();
-                        JOptionPane.showMessageDialog(null, Double.parseDouble(temperatura) +
-                                " grados Farenheit equivalen a " + grados + " grados Celsius");
-                        break;
-                    case "Grados Kelvin a grados Celsius":
-                        Temperatura Celsius2 = new Temperatura("Celsius", Double.parseDouble(temperatura));
-                        grados = Celsius2.doKelvinToCelsiusConversion();
-                        JOptionPane.showMessageDialog(null, Double.parseDouble(temperatura) +
-                                " grados Kerlvin equivalen a " + grados + " grados Celsius");
-                        break;
+                catch (NullPointerException ex){
+                    JOptionPane.showMessageDialog(null, "No puedes dejar el campo vacío");
                 }
             }
             option = JOptionPane.showConfirmDialog(null, "Quieres salir del programa?");
         }
         while(option != 0);
         JOptionPane.showMessageDialog(null, "Has salido del programa");
-        return;
     }
 }
